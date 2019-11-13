6c788eadd22e9efae025f424f4e0ae582a97b3b4
ninhydrin <the.nocturn.of.daybreak@gmail.com>
2019-11-13 11:28:45 +0900

    fix

--- keybindings/keybind_maker.py
+++ keybindings/keybind_maker.py
@@ -1,6 +1,7 @@
 # encoding:utf-8
 import json
 import os
+from pathlib import Path
 import re
 
 modifier = {
@@ -12,13 +13,14 @@ modifier = {
 def make_keybindings_file():
     """create keybindings"""
 
-    home_dir = os.environ["HOME"]
-    keybindings_json = "Library/Application Support/Code/User/keybindings.json"
-    vscode_keybindings_path = os.path.join(home_dir, keybindings_json)
+    home_dir = Path.home()
+    keybindings_json = Path("Library/Application Support/Code/User/keybindings.json")
+    vscode_keybindings_path = home_dir / keybindings_json
     key_bind_json = []
-    json_dir = os.path.dirname(os.path.abspath(__file__))
-    bind_json_list = [i for i in os.listdir(json_dir) if re.search("[0-9]*", i).group(0)]
-    for i in [os.path.join(json_dir, f_name) for f_name in bind_json_list]:
+    # json_dir = os.path.dirname(os.path.abspath(__file__))
+    json_dir: Path = Path.absolute(Path(__file__).parent)
+    bind_json_list = [i for i in json_dir.iterdir() if re.search("[0-9]*", i.name).group(0)]
+    for i in bind_json_list:
         with open(i) as f:
             binds = json.load(f)
         for bind in binds:
