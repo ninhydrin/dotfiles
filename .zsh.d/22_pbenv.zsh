0ccc6f6c61dae4d64ca232f7d7322808e624391b
ninhydrin <the.nocturne.of.daybreak@gmail.com>
2020-09-10 09:45:30 +0900

    read -k

--- .zsh.d/22_pbenv.zsh
+++ .zsh.d/22_pbenv.zsh
@@ -1,8 +1,8 @@
 if [ ! -e $HOME/.rbenv ]; then
     echo "There is not .rbenv. Do you clone rbenv ? [y/n]"
-    read ANSWER
+    read -k 1 ANSWER
     case $ANSWER in
-        "" | "Y" | "y" | "yes" | "Yes" | "YES" )
+        "Y" | "y" | "yes" | "Yes" | "YES" )
             case ${OSTYPE} in
                 darwin*)
                     brew install rbenv
