3aa93951e71058f62f606ea10c4dc6ea4222a988
ninhydrin <the.nocturne.of.daybreak@gmail.com>
2021-01-07 23:17:23 +0900

    add peco

---
+++ .zsh.d/23_peco.zsh
@@ -0,0 +1,29 @@
+which peco
+if [ $? = 0  ]; then
+
+else
+    echo "not installed peco. install? [Y/n]"
+    read -k 1 ANSWER
+    case $ANSWER in "Y" | "y" | "yes" | "Yes" | "YES" )
+        brew install peco
+        ;;
+    esac
+fi
+which peco
+if [ $? = 0  ]; then
+    function peco-select-history() {
+        local tac
+        if which tac > /dev/null; then
+            tac="tac"
+        else
+            tac="tail -r"
+        fi
+        BUFFER=$(\history -n 1 | \
+            eval $tac | \
+            peco --query "$LBUFFER")
+        CURSOR=$#BUFFER
+        zle clear-screen
+    }
+    zle -N peco-select-history
+    bindkey '^r' peco-select-history
+fi
