7f292fce966224d479488b749958fd25317673f8
ninhydrin <the.nocturne.of.daybreak@gmail.com>
2022-03-05 10:10:57 +0900

    newenv command

--- .zsh.d/21_pyenv.zsh
+++ .zsh.d/21_pyenv.zsh
@@ -22,21 +22,20 @@ if [ ! -e ${PYENV_ROOT} -a ! -f $ZSH_VARIABLES/no_pyenv ]; then
     esac
 fi
 
-alias newenv="echo pyenv not installed!!"
+# alias newenv="echo pyenv not installed!!"
 if [ -d "${PYENV_ROOT}" ]; then
     export PATH=${PYENV_ROOT}/bin:$PATH
     eval "$(pyenv init -)"
-	alias newenv="pyenv virtualenv --python 3.8.5 miniconda3-latest "
-	if [ ! -d ${PYENV_ROOT}/versions/miniconda3-latest ]; then
-		echo "not installed miniconda3-latest. install? [Y/n]"
-		read -k 1 ANSWER
-		case $ANSWER in "Y" | "y" | "yes" | "Yes" | "YES" )
-		pyenv install miniconda3-latest
-		# conda install -y -c conda-forge opencv
-		# pip install opencv-python
-		alias newenv="pyenv virtualenv --python 3.7.4 miniconda3-latest "
-		;;
-		* ) echo "miniconda not installed";;
-		esac
-	fi
+    newenv(){
+        if [ $# -eq 0 ]; then
+            echo "input new python env name"
+        elif [ $# -eq 1 ]; then
+            # pyenv virtualenv --python 3.10.0 miniconda3-latest $1
+            pyenv virtualenv --python 3.9.0 miniconda3-latest $1
+            pyenv local $1
+            pyenv rehash
+        else
+            pyenv virtualenv --python 3.9.0 miniconda3-latest $1
+        fi
+    }
 fi
