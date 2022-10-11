f94fac263445d56852dc6aa97f3042ef9bbca133
kura_yokoshima <kura_yokoshima@dwango.co.jp>
2022-10-11 09:21:36 +0900

    resource

--- .zsh.d/9_alias.zsh
+++ .zsh.d/9_alias.zsh
@@ -61,6 +61,7 @@ alias amadeus="python ~/amadeus/job.py"
 alias ks="kubectl get svc"
 alias kj="kubectl get jobs"
 alias kp="kubectl get pods"
+alias kr='kubectl get ResourceQuota --all-namespaces'
 
 ## lsとpsの設定
 ### ls: できるだけGNU lsを使う。
