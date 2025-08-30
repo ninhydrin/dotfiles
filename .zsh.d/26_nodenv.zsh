b9a5ee79f3274f9d09aec378ae3d080a01fd4f5f
ninhydrin <kura_yokoshima@dwango.co.jp>
2025-08-30 17:58:30 +0900

    wip

--- .zsh.d/26_nodenv.zsh
+++ .zsh.d/26_nodenv.zsh
@@ -1,3 +1,5 @@
 # git clone https://github.com/nodenv/nodenv.git ~/.nodenv
 # mkdir ~/.nodenv/plugins
-# git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins
+# git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
+# arch -x86_64 nodenv install 15.6.0
+# npm install -g yarn
