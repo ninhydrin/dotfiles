4f1798c0654ed0d0812d52a016654d5421a0d049
ninhydrin <kura_yokoshima@dwango.co.jp>
2025-08-31 10:50:08 +0900

    uv install

--- .zsh.d/17_uv.zsh
+++ .zsh.d/17_uv.zsh
@@ -1,4 +1,28 @@
 
+if [ ! command -v uv > /dev/null 2>&1 -a ! -f $ZSH_VARIABLES/no_uv ]; then
+    echo "uvがインストールされていません。uvをインストールしますか？ [y/n]"
+    read -k 1 ANSWER
+    case $ANSWER in
+        "Y" | "y" | "yes" | "Yes" | "YES" )
+            echo
+            case ${OSTYPE} in
+                darwin*)
+                    curl -LsSf https://astral.sh/uv/install.sh | sh
+                ;;
+                linux*)
+                    curl -LsSf https://astral.sh/uv/install.sh | sh
+                ;;
+            esac
+            ;;
+        "N" | "No" | "NO" )
+            echo
+            touch $ZSH_VARIABLES/no_uv ;;
+        "n" | "no" )
+            echo ;;
+        * ) ;;
+    esac
+fi
+
 venv() {
     if [ -d ".venv" ]; then
         source .venv/bin/activate
