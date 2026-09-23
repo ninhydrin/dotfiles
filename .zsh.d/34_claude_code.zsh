b15f91b1b758697b047dd0b880cbe07b01bf2cb3
ninhydrin <the.nocturne.of.daybreak@gmail.com>
2026-09-23 23:31:16 +0900

    change 5.5

--- .zsh.d/34_claude_code.zsh
+++ .zsh.d/34_claude_code.zsh
@@ -1,5 +1,5 @@
 export CLAUDE_CODE_NO_FLICKER=1
 alias dclaude='IS_SANDBOX=1 claude --dangerously-skip-permissions'
-export ANTHROPIC_DEFAULT_OPUS_MODEL="claude-opus-5[1m]"
+export ANTHROPIC_DEFAULT_OPUS_MODEL="claude-opus-5-5[1m]"
 export PATH="${HOME}/global_commands/.venv/bin:$PATH"
 export HEADROOM_TELEMETRY=off
